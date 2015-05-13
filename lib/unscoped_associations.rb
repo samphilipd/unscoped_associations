be5600fc22854a9b1ced3f5812eda3a9780b5880
Marc Anguera Insa <srmarc.ai@gmail.com>
2015-05-14 01:50:41 +0200

    refactor (use alias_method_chain instead of method override); more natural way to include Module into AR

--- lib/unscoped_associations.rb
+++ lib/unscoped_associations.rb
@@ -3,7 +3,7 @@ require 'unscoped_associations/version'
 module UnscopedAssociations
   def self.included(base)
     base.extend ClassMethods
-    (class << base; self; end).instance_eval do
+    class << base
       alias_method_chain :belongs_to, :unscoped
       alias_method_chain :has_many, :unscoped
       alias_method_chain :has_one, :unscoped
@@ -11,51 +11,47 @@ module UnscopedAssociations
   end
 
   module ClassMethods
-    def belongs_to_with_unscoped(name, scope = nil, options = {})
-      build_unscoped(:belongs_to, name, scope, options)
+    def belongs_to_with_unscoped(target, scope = nil, options = {})
+      build_unscoped(:belongs_to, target, scope, options)
     end
 
-    def has_many_with_unscoped(name, scope = nil, options = {}, &extension)
-      build_unscoped(:has_many, name, scope, options, &extension)
+    def has_many_with_unscoped(target, scope = nil, options = {}, &extension)
+      build_unscoped(:has_many, target, scope, options, &extension)
     end
 
-    def has_one_with_unscoped(name, scope = nil, options = {})
-      build_unscoped(:has_one, name, scope, options)
+    def has_one_with_unscoped(target, scope = nil, options = {})
+      build_unscoped(:has_one, target, scope, options)
     end
 
     private
 
-    def build_unscoped(assoc_type, assoc_name, scope = nil, options = {}, &extension)
+    def build_unscoped(assoc_type, target, scope = nil, options = {}, &extension)
       if scope.is_a?(Hash)
         options = scope
         scope   = nil
       end
 
-      if options.delete(:unscoped)
-        add_unscoped_association(assoc_name)
-      end
+      apply_unscoped = options.delete(:unscoped)
 
       if scope
-        self.send("#{assoc_type}_without_unscoped", assoc_name, scope, options, &extension)
+        send("#{assoc_type}_without_unscoped", target, scope, options, &extension)
       else
-        self.send("#{assoc_type}_without_unscoped", assoc_name, options, &extension)
+        send("#{assoc_type}_without_unscoped", target, options, &extension)
       end
+
+      unscope_association(target) if apply_unscoped
     end
 
-    def add_unscoped_association(association_name)
-      define_method(association_name) do
-        if self.class.reflect_on_association(association_name).options.key?(:polymorphic)
-          self.association(association_name).klass.unscoped do
-            super(association_name)
-          end
-        else
-          self.class.reflect_on_association(association_name).klass.unscoped do
-            super(association_name)
-          end
+    def unscope_association(target)
+      define_method("#{target}_with_unscoped") do |*args|
+        association(target).klass.unscoped do
+          send("#{target}_without_unscoped", args)
         end
       end
+
+      alias_method_chain target, :unscoped
     end
   end
 end
 
-ActiveRecord::Base.instance_eval { include UnscopedAssociations }
+ActiveRecord::Base.send(:include, UnscopedAssociations)
